97b4676a42dea1e8f5ae3f1431d22ab8e4b5a193
İbrahim Gün <ibrahimggun@gmail.com>
2021-10-09 14:09:12 +0300

    add high order functions

--- Today/ReminderListDataSource.swift
+++ Today/ReminderListDataSource.swift
@@ -8,24 +8,56 @@
 import UIKit
 
 class ReminderListDataSource: NSObject {
-    private lazy var dateFormatter = RelativeDateTimeFormatter()
+    
     
     enum Filter: Int {
         case today
         case future
         case all
+        
+        func shouldInclude(date: Date) -> Bool {
+            let isInToday = Locale.current.calendar.isDateInToday(date)
+            switch self {
+            case .today:
+                return isInToday
+            case .future:
+                return (date > Date()) && !isInToday
+            case .all:
+                return true
+            }
+        }
+    }
+    
+    var filter: Filter = .today
+    var filteredReminders: [Reminder] {
+        return Reminder.testData.filter { filter.shouldInclude(date: $0.dueDate) }.sorted { $0.dueDate < $1.dueDate }
     }
     
     func update(_ reminder: Reminder, at row: Int) {
-        Reminder.testData[row] = reminder
+        let index = self.index(for: row)
+        Reminder.testData[index] = reminder
+    }
+    
+    func delete(at row: Int) {
+        let index = self.index(for: row)
+        Reminder.testData.remove(at: index)
     }
     
     func reminder(at row: Int) -> Reminder {
-        return Reminder.testData[row]
+        return filteredReminders[row]
     }
     
-    func add(_ reminder: Reminder) {
+    func add(_ reminder: Reminder) -> Int? {
         Reminder.testData.insert(reminder, at: 0)
+        return filteredReminders.firstIndex(where: { $0.id == reminder.id })
+    }
+    
+    func index(for filteredIndex: Int) -> Int {
+        let filteredReminder = filteredReminders[filteredIndex]
+        guard let index = Reminder.testData.firstIndex(where: { $0.id == filteredReminder.id }) else {
+            fatalError("Couldn't retrieve index in source array")
+        }
+        return index
     }
     
 }
@@ -34,7 +66,7 @@ extension ReminderListDataSource: UITableViewDataSource {
     static let reminderListCellIdentifier = "ReminderListCell"
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return Reminder.testData.count
+        return filteredReminders.count
         
     }
     
@@ -42,12 +74,65 @@ extension ReminderListDataSource: UITableViewDataSource {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: Self.reminderListCellIdentifier, for: indexPath) as? ReminderListCell else {
             fatalError("Unable to deqeue ReminderCell")
         }
-        let reminder = Reminder.testData[indexPath.row]
-        let dateText = dateFormatter.localizedString(for: reminder.dueDate, relativeTo: Date())
-        cell.configure(title: reminder.title, dateText: dateText, isDone: reminder.isComplete) {
-            Reminder.testData[indexPath.row].isComplete.toggle()
+        let currentReminder = reminder(at: indexPath.row)
+        let dateText = currentReminder.dueDateTimeText(for: filter)
+        cell.configure(title: currentReminder.title, dateText: dateText, isDone: currentReminder.isComplete) {
+            var modifiedReminder = currentReminder
+            modifiedReminder.isComplete.toggle()
+            self.update(modifiedReminder, at: indexPath.row)
             tableView.reloadRows(at: [indexPath], with: .none)
-        }
+                }
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        guard editingStyle == .delete else {
+            return
+        }
+        delete(at: indexPath.row)
+        tableView.performBatchUpdates({
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }) { (_) in
+            tableView.reloadData()
+        }
+    }
+}
+
+extension Reminder {
+    static let timeFormatter: DateFormatter = {
+        let timeFormatter = DateFormatter()
+        timeFormatter.dateStyle = .none
+        timeFormatter.timeStyle = .short
+        return timeFormatter
+    }()
+    
+    static let futureDateFormatter: DateFormatter = {
+       let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .short
+        return dateFormatter
+    }()
+    
+    static let todayDateFormatter: DateFormatter = {
+       let format = NSLocalizedString("'Today at '%@", comment: "format string for dates occuring today")
+       let dateFormatter = DateFormatter()
+       dateFormatter.dateFormat = String(format: format, "hh:mm a")
+       return dateFormatter
+    }()
+    
+    func dueDateTimeText(for filter: ReminderListDataSource.Filter) -> String {
+        let isInToday = Locale.current.calendar.isDateInToday(dueDate)
+        switch filter {
+        case .today:
+            return Self.timeFormatter.string(from: dueDate)
+        case .future:
+            return Self.futureDateFormatter.string(from: dueDate)
+        case .all:
+            if isInToday {
+                return Self.todayDateFormatter.string(from: dueDate)
+            } else {
+                return Self.futureDateFormatter.string(from: dueDate)
+            }
+        }
     }
 }
--- Today/ReminderListViewController.swift
+++ Today/ReminderListViewController.swift
@@ -8,11 +8,16 @@
 import UIKit
 
 class ReminderListViewController: UITableViewController {
+    @IBOutlet var filterSegmentedControl: UISegmentedControl!
+    
     
     private var reminderListDataSource: ReminderListDataSource?
+    private var filter: ReminderListDataSource.Filter {
+        return ReminderListDataSource.Filter(rawValue: filterSegmentedControl.selectedSegmentIndex) ?? .today
+    }
+    
     static let mainStoryboardName = "Main"
     static let detailViewControllerIdentifier = "ReminderDetailViewController"
-    
     static let showDetailSegueIdentifier = "ShowReminderDetailSegue"
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -26,7 +31,7 @@ class ReminderListViewController: UITableViewController {
             }
             destination.configure(with: reminder, editAction: { reminder in
                 self.reminderListDataSource?.update(reminder, at: rowIndex)
-                self.tableView.reloadRows(at: [indexPath], with: .automatic)
+                self.tableView.reloadData()
             })
         }
     }
@@ -49,13 +54,20 @@ class ReminderListViewController: UITableViewController {
         addReminder()
     }
     
+    @IBAction func segmentControlChanged(_ sender: UISegmentedControl) {
+        reminderListDataSource?.filter = filter
+        tableView.reloadData()
+    }
+    
+    
     private func addReminder() {
         let storyboard = UIStoryboard(name: Self.mainStoryboardName, bundle: nil)
         let detailViewController: ReminderDetailViewController = storyboard.instantiateViewController(identifier: Self.detailViewControllerIdentifier)
-        let reminder = Reminder(title: "New Reminder", dueDate: Date())
+        let reminder = Reminder(id: UUID().uuidString, title: "New Reminder", dueDate: Date())
         detailViewController.configure(with: reminder, isNew: true, addAction: { reminder in
-            self.reminderListDataSource?.add(reminder)
-            self.tableView.insertRows(at: [IndexPath(row: 0, section: 0)], with: .automatic)
+            if let index = self.reminderListDataSource?.add(reminder) {
+                self.tableView.insertRows(at: [IndexPath(row: index, section: 0)], with: .automatic)
+            }
             
         })
         
